50ac7b00c31e548e7d7f4544085ee31a23251b8a
Artem Purundzhan <artempa1607@gmail.com>
2025-01-17 10:19:31 +0100

    feat: add ui changes

--- index.ts
+++ index.ts
@@ -10,6 +10,9 @@ import type {
   DispatchResult,
   DataItem,
 } from 'arconnect';
+import Lottie from 'lottie-web';
+import PaperplaneAnimation from './public/assets/paperplane.json';
+import pattern from './pettern';
 
 interface WalletResponse {
   action?: string;
@@ -28,11 +31,33 @@ interface ResponseListenerData {
   resolve: (response: any) => void;
 }
 
+const preconnectGoogleFonts = document.createElement('link');
+preconnectGoogleFonts.rel = 'preconnect';
+preconnectGoogleFonts.href = 'https://fonts.googleapis.com';
+document.head.appendChild(preconnectGoogleFonts);
+
+// Create <link> for preconnect to fonts.gstatic.com with crossorigin
+const preconnectGstatic = document.createElement('link');
+preconnectGstatic.rel = 'preconnect';
+preconnectGstatic.href = 'https://fonts.gstatic.com';
+preconnectGstatic.crossOrigin = 'anonymous'; // Specify crossorigin
+document.head.appendChild(preconnectGstatic);
+
+// Create <link> for the actual font stylesheet
+const fontStylesheet = document.createElement('link');
+fontStylesheet.rel = 'stylesheet';
+fontStylesheet.href =
+  'https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap';
+document.head.appendChild(fontStylesheet);
+
+const incodedPattern = btoa(pattern);
+
 class WalletClient {
   private client: MqttClient | null;
   private uid: string | null;
   private qrCode: Promise<string> | null;
   private modal: HTMLDivElement | null;
+  private approvalModal: HTMLDivElement | null;
   private responseListeners: Map<string, ResponseListenerData>;
   private connectionListener: ((response: any) => void) | null;
   private responseTimeoutMs: number;
@@ -45,6 +70,7 @@ class WalletClient {
     this.uid = null;
     this.qrCode = null;
     this.modal = null;
+    this.approvalModal = null;
     this.responseListeners = new Map();
     this.connectionListener = null;
     this.responseTimeoutMs = responseTimeoutMs;
@@ -56,7 +82,6 @@ class WalletClient {
   private createModal(qrCodeData: string, styles?: ModalStyles): void {
     if (this.modal) return;
 
-    // Create modal backdrop
     const modal = document.createElement('div');
     Object.assign(modal.style, {
       position: 'fixed',
@@ -77,18 +102,33 @@ class WalletClient {
       background: 'white',
       borderRadius: '16px',
       padding: '28px',
+      paddingTop: '47px',
       textAlign: 'center',
       minWidth: '300px',
-      fontFamily: 'system-ui, -apple-system, sans-serif',
+      fontFamily: 'Sora',
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      alignItems: 'center',
+      backgroundRepeat: 'no-repeat',
     });
 
+    content.style.backgroundImage = `url(${
+      'data:image/svg+xml;base64,' + incodedPattern
+    })`;
+
     content.innerHTML = `
-      <h3 style="color: #000; font-size: 18px; font-weight: 500; margin-bottom: 5px;">AOSync</h3>
+     <div style="width: 50px; height: 50px; border-radius: 50%; background-color: #F7FAFD; margin-bottom: 12px; display: flex; justify-content: center; align-items: center">
+      <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 0.8H0.8V1V4.40476V4.60476H1H1.66667H1.86667V4.40476V1.81905H4.66667H4.86667V1.61905V1V0.8H4.66667H1ZM11.3333 0.8H11.1333V1V1.61905V1.81905H11.3333H14.1333V4.40476V4.60476H14.3333H15H15.2V4.40476V1V0.8H15H11.3333ZM1 10.3952H0.8V10.5952V14V14.2H1H4.66667H4.86667V14V13.381V13.181H4.66667H1.86667V10.5952V10.3952H1.66667H1ZM14.3333 10.3952H14.1333V10.5952V13.181H11.3333H11.1333V13.381V14V14.2H11.3333H15H15.2V14V10.5952V10.3952H15H14.3333Z" fill="black" stroke="black" stroke-width="0.4"/>
+      </svg>
+      </div>
+      <h3 style="color: #000; font-size: 18px; font-weight: 500; margin-bottom: 5px; margin-top: 0px">AOSync</h3>
       <div style="font-size: 14px; color: #2B2B2B; margin-bottom: 2px;">Scan with your beacon wallet</div>
-      <img src="${qrCodeData}" alt="QR Code" style="width: 200px; height: 200px; margin-bottom: 10px;">
+      <img id="aosync-beacon-connection-qrCode" src="${qrCodeData}" alt="QR Code" style="width: 200px; height: 200px; margin-bottom: 10px;">
       <div style="font-size: 11px; color: #2B2B2B;">
-        Don't have beacon yet?
-        <a href="https://beaconwallet.com" 
+        <span id="aosync-beacon-modal-description">Don't have beacon yet?</span>
+        <a href="https://beaconwallet.app" 
            target="_blank" 
            style="color: #09084B; text-decoration: none; display: block; margin-top: 8px;">
           beaconwallet.com
@@ -100,42 +140,79 @@ class WalletClient {
     this.modal = modal;
   }
 
-  private createModalContent(
-    qrCodeData: string,
-    styles?: ModalStyles
-  ): HTMLDivElement {
+  private createApprovalModal(): void {
+    if (this.approvalModal) return;
+
+    const modal = document.createElement('div');
+    Object.assign(modal.style, {
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      zIndex: '999999',
+    });
+
     const content = document.createElement('div');
     Object.assign(content.style, {
-      backgroundColor: '#fff',
-      padding: styles?.padding || '20px',
-      borderRadius: '23px',
-      boxShadow: '0 4px 10px rgba(0, 0, 0, 0.25)',
+      background: 'white',
+      borderRadius: '16px',
+      padding: '28px',
+      paddingTop: '47px',
       textAlign: 'center',
-      color: '#000',
+      minWidth: '300px',
+      fontFamily: 'system-ui, -apple-system, sans-serif',
       display: 'flex',
       justifyContent: 'center',
       flexDirection: 'column',
+      alignItems: 'center',
+      backgroundRepeat: 'no-repeat',
     });
 
-    const text = document.createElement('p');
-    text.textContent = 'Scan the QR Code to connect:';
-    text.style.marginBottom = '20px';
+    content.style.backgroundImage = `url(${
+      'data:image/svg+xml;base64,' + incodedPattern
+    })`;
 
-    const qrImg = document.createElement('img');
-    Object.assign(qrImg, {
-      id: 'qr-code',
-      alt: 'QR Code',
-      src: qrCodeData,
-      style: {
-        maxWidth: '180px',
-        height: 'auto',
-      },
-    });
+    content.innerHTML = `
+     <div style="width: 50px; height: 50px; border-radius: 50%; background-color: #F7FAFD; margin-bottom: 12px; display: flex; justify-content: center; align-items: center">
+      <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 0.8H0.8V1V4.40476V4.60476H1H1.66667H1.86667V4.40476V1.81905H4.66667H4.86667V1.61905V1V0.8H4.66667H1ZM11.3333 0.8H11.1333V1V1.61905V1.81905H11.3333H14.1333V4.40476V4.60476H14.3333H15H15.2V4.40476V1V0.8H15H11.3333ZM1 10.3952H0.8V10.5952V14V14.2H1H4.66667H4.86667V14V13.381V13.181H4.66667H1.86667V10.5952V10.3952H1.66667H1ZM14.3333 10.3952H14.1333V10.5952V13.181H11.3333H11.1333V13.381V14V14.2H11.3333H15H15.2V14V10.5952V10.3952H15H14.3333Z" fill="black" stroke="black" stroke-width="0.4"/>
+      </svg>
+      </div>
+      <h3 style="color: #000; font-size: 18px; font-weight: 500; margin-bottom: 5px; margin-top: 0px">AOSync</h3>
+      <div style="font-size: 14px; color: #2B2B2B; margin-bottom: 2px;">Approval pending ...</div>
+      <div id="aosync-lottie-animation" style="width: 200px; height: 200px; margin-bottom: 10px;"></div>
+      <div style="font-size: 11px; color: #2B2B2B;">
+        <a href="https://beaconwallet.app" 
+           target="_blank" 
+           style="color: #09084B; text-decoration: none; display: block; margin-top: 8px;">
+          beaconwallet.com
+        </a>
+      </div>
+    `;
+    modal.appendChild(content);
+    document.body.appendChild(modal);
+    this.approvalModal = modal;
 
-    const closeButton = this.createCloseButton();
-
-    content.append(text, qrImg, closeButton);
-    return content;
+    const lottieContainer = document.getElementById('aosync-lottie-animation');
+    if (lottieContainer) {
+      try {
+        Lottie.loadAnimation({
+          container: lottieContainer,
+          renderer: 'svg',
+          loop: true,
+          autoplay: true,
+          path: '../paperplane.json',
+          animationData: PaperplaneAnimation,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 
   private createCloseButton(): HTMLButtonElement {
@@ -156,10 +233,49 @@ class WalletClient {
     return button;
   }
 
+  private connectionModalSuccessMessage(): void {
+    const qrCode = document.getElementById('aosync-beacon-connection-qrCode');
+
+    const modalDescription = document.getElementById(
+      'aosync-beacon-modal-description'
+    );
+    const successMark = document.createElement('div');
+    Object.assign(successMark.style, {
+      width: '200px',
+      height: '200px',
+      marginBottom: '10px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingBottom: '30px',
+      boxSizing: 'border-box',
+    });
+    if (modalDescription) {
+      modalDescription!.style.visibility = 'hidden';
+    }
+    successMark.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="79" height="57" viewBox="0 0 79 57" fill="none">
+      <path d="M26.9098 57L0 30.221L5.18687 25.0593L26.9098 46.7012L73.8391 0L79 5.16166L26.9098 57Z" fill="#27BD69"/>
+    </svg>
+    `;
+    qrCode?.replaceWith(successMark);
+
+    setTimeout(() => {
+      this.closeModal();
+    }, 1000);
+  }
+
   private closeModal(): void {
     if (this.modal) {
       document.body.removeChild(this.modal);
       this.modal = null;
+    }
+  }
+
+  private closeApprovalModal(): void {
+    if (this.approvalModal) {
+      document.body.removeChild(this.approvalModal);
+      this.approvalModal = null;
     }
   }
 
@@ -174,7 +290,7 @@ class WalletClient {
     const messageData = JSON.parse(message.toString()) as WalletResponse;
 
     if (messageData.action === 'connect') {
-      this.closeModal();
+      this.connectionModalSuccessMessage();
       await this.handleConnectResponse(packet);
       return;
     }
@@ -187,11 +303,18 @@ class WalletClient {
     const correlationId = packet?.properties?.correlationData?.toString();
     if (correlationId && this.responseListeners.has(correlationId)) {
       const listenerData = this.responseListeners.get(correlationId)!;
+      const isTransaction = ['sign', 'dispatch', 'signDataItem'].includes(
+        listenerData.action
+      );
       if (listenerData.action === 'signDataItem') {
-        const decodedData = this.base64UrlDecode(messageData.data)
-        listenerData.resolve(decodedData)
+        const decodedData = this.base64UrlDecode(messageData.data);
+        listenerData.resolve(decodedData);
       } else {
         listenerData.resolve(messageData.data);
+      }
+
+      if (isTransaction) {
+        this.closeApprovalModal();
       }
       this.responseListeners.delete(correlationId);
     }
@@ -287,6 +410,10 @@ class WalletClient {
           this.responseListeners.delete(correlationData);
           reject(err);
         });
+      }
+
+      if (isTransaction) {
+        this.createApprovalModal();
       }
 
       const timeout = setTimeout(() => {
