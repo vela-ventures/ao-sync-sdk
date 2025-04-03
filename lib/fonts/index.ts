3b7e292b287d0d9d64a6a382fa890227136f9292
artempdev <artempa1607@gmail.com>
2025-04-03 12:11:24 +0100

    chore: udpate colors, fonts loading

--- lib/fonts/index.ts
+++ lib/fonts/index.ts
@@ -1,3 +1,22 @@
+if (typeof window !== "undefined") {
+  const preconnectGoogleFonts = document.createElement("link");
+  preconnectGoogleFonts.rel = "preconnect";
+  preconnectGoogleFonts.href = "https://fonts.googleapis.com";
+  document.head.appendChild(preconnectGoogleFonts);
+
+  const preconnectGstatic = document.createElement("link");
+  preconnectGstatic.rel = "preconnect";
+  preconnectGstatic.href = "https://fonts.gstatic.com";
+  preconnectGstatic.crossOrigin = "anonymous";
+  document.head.appendChild(preconnectGstatic);
+
+  const fontStylesheet = document.createElement("link");
+  fontStylesheet.rel = "stylesheet";
+  fontStylesheet.href =
+    "https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap";
+  document.head.appendChild(fontStylesheet);
+}
+
 import "@fontsource/sora";
 import "@fontsource/sora/400.css";
 import "@fontsource/sora/500.css";
